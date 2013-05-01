6678c416d55f78c223694ab089f5df440756ee56
Patrick Schoenmakers <pschoenmakers@qualogy.com>
2013-05-01 16:45:34 +0200

    Wat een gedoe

---
+++ src/test/java/org/geenz/selenium/LoginTest.java
@@ -0,0 +1,111 @@
+package org.geenz.selenium;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class LoginTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:9000";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testLogin() throws Exception {
+    driver.get(baseUrl + "/login");
+
+    login("guillaume@sample.com","secret");
+
+    try {
+      assertTrue(driver.findElement(By.id("user")).getText().matches("^Guillaume Bort[\\s\\S]*$"));
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    
+    logout();
+  }
+
+  @Test
+  public void testWrongLogin() throws Exception {
+    driver.get(baseUrl + "/login");
+    System.out.println("kiipppe");
+
+    login("guillaume@sample.com","kippesoep");
+
+    assertTrue(driver.findElement(By.cssSelector("p[class=\"error\"]")).getText().matches("^Invalid user or password[\\s\\S]*$"));
+
+    /*
+    try {
+      assertTrue(driver.findElement(By.cssSelector("p[class=\"error\"]")).getText().matches("^VLAFLIPInvalid user or password[\\s\\S]*$"));
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    logout();
+    */
+  }
+
+  public void login(String email, String password) throws Exception {
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys(email);
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys(password);
+    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+  }
+
+  public void logout() throws Exception {
+    driver.findElement(By.linkText("Logout")).click();
+  }
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
