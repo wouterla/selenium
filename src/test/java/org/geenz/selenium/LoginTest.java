34a3a5e187fa9b737c67556f2f1e8c8c493c5646
florimon <florimon@florimon.net>
2013-05-01 16:46:15 +0200

    Initial creation of LoginTest.java .

---
+++ src/test/java/org/geenz/selenium/LoginTest.java
@@ -0,0 +1,90 @@
+package org.geenz.selenium;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class LoginTest {
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "http://localhost:9000";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testLogin() throws Exception {
+		login("guillaume@sample.com", "secret");
+		assertEquals(getLoggedInUser(), "(guillaume@sample.com)");
+		logout();
+	}
+
+	private void logout() {
+		driver.findElement(By.linkText("Logout")).click();
+	}
+
+	private void login(String email, String password) {
+		driver.get(baseUrl + "/login");
+		driver.findElement(By.name("email")).clear();
+		driver.findElement(By.name("email")).sendKeys(email);
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+	}
+	
+	public String getLoggedInUser() {
+		return driver.findElement(By.cssSelector("#user span")).getText();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+}
